20d096e7d1a54b99a606dd3b0d0a74048b812d91
김동욱 <ehddnr73@naver.com>
2024-03-11 16:26:49 +0900

    type 및 interface

--- restaurant-app/src/App.tsx
+++ restaurant-app/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Store from './Store';
+import { Address, Restaurant } from './model/restaurant';
 
-function App() {
+let data: Restaurant = {
+  name: "yeolmok", 
+  category: "", 
+  address: {
+    city: "seoul",
+    detail: "somewhere", 
+    zipCode: 123
+  }, 
+  menu: [{name: "fried", price: 15000, category: "chicken"}]
+}
+
+const App:React.FC = () => {
+  const [restaurant, setRestaurent] = useState<Restaurant>(data);
+
+  const changeAddress = (address: Address) => {
+    setRestaurent({...restaurant, address: address});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Store info={restaurant} changeAddress={changeAddress}/>
     </div>
   );
 }
